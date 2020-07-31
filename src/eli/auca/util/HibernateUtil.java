8443e2c818f92f3e1c669561e779a61628cba0af
Eli <Eli@DESKTOP-K85V2EN>
2020-07-31 15:33:03 +0200

    This is my Library Management System Demo The project is still undergoing.
    Please review my project for improvement.

--- src/eli/auca/util/HibernateUtil.java
+++ src/eli/auca/util/HibernateUtil.java
@@ -12,7 +12,7 @@ import org.hibernate.SessionFactory;
  * Hibernate Utility class with a convenient method to get Session Factory
  * object.
  *
- * @author Eli-PC
+ * @author Eli
  */
 public class HibernateUtil {
 
